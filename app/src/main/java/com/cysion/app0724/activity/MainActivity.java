716fd9be84f556e68284acb5ae4cd17a2c9f572d
Cysion <liuxshang@163.com>
2016-10-16 00:33:34 +0800

    reset ok?

--- app/src/main/java/com/cysion/app0724/activity/MainActivity.java
+++ app/src/main/java/com/cysion/app0724/activity/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         initMenuList();
         //vp
         initViewPager();
-        // for testing
+        // reset ok?
     }
 
     private void initMenuList() {
